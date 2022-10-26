4fc2c4910dc41c58caa91345114d8d02b3a9a928
admin <admin@MacBook-Pro-de-admin-78.local>
2022-10-26 15:46:27 +0200

    add logout page

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CONNEXION</title>
     <link rel="stylesheet" href="assets/css/header.css">
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/footer.css">
@@ -29,7 +29,7 @@
             <input class="input_class" name="email" type="text" placeholder="Email" id="email" required>
             <input class="input_class" name="password" type="password" placeholder="Mot de passe" id="password" required>
 
-            <button type="submit" id="button_login" action="./action_register.php">Connexion</button>
+            <button type="submit" id="submit_login" name="submit_login">Connexion</button>
         </form>
 
     </section>
--- logout.php
+++ logout.php
@@ -1,6 +1,44 @@
-<?php 
+<?php
 require('./includes/init.php');
-session_unset();
-session_destroy();
-echo 'Vous avez été déconnecté <a href="./index.php">Retour</a>';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DECONNEXION | THE POWER OF MEMORY</title>
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/logout.css">
+</head>
+
+<body>
+    <section class="logout">
+        <?php require('./view/header.inc.php'); ?>
+        <section class="titre_background register_background">
+            <section id="titre_page">
+                <h2 class="h2_page">DECONNEXION</h2>
+            </section>
+        </section>
+
+        <p class="logout_message">
+            Vous avez bien été déconnecté. Merci pour votre visite !
+        </p>
+        <p class="logout_message">
+            Cliquez <a href="./index.php">ici</a> pour être rediriger vers la page d'accueil.
+        </p>
+    </section>
+
+    <?php
+    require('./view/footer.inc.php');
+    session_unset();
+    session_destroy();
+    ?>
+
+</body>
+
+</html>
